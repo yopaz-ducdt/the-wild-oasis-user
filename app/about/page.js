e27deb3a69ec228d0eb95c04f183cbe06049cb57
Dao Trung Duc <daotrungduc.it@gmail.com>
2025-11-10 08:46:30 +0700

    first commit

---
+++ app/about/page.js
@@ -0,0 +1,11 @@
+export const metadata = {
+  title: "About",
+};
+
+export default function Home() {
+  return (
+    <>
+      <h1>About Page</h1>
+    </>
+  );
+}
---
+++ app/account/page.js
@@ -0,0 +1,11 @@
+export const metadata = {
+  title: "Guest area",
+};
+
+export default function Home() {
+  return (
+    <>
+      <h1>Account Page</h1>
+    </>
+  );
+}
